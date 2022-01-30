a0e0a79a0d58def6b6903d8d1e595c19a21f7512
isaacpitwa <isaacpitwa256@gmail.com>
2022-01-30 13:14:48 +0300

    Feat: map Circles example

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:custom_info_window/custom_info_window.dart';
 
 import 'models/map_style.dart';
+import 'pages/map_circles.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const MapCircles(),
     );
   }
 }
